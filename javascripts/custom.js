093125125804440e53dce1504516c821364f1120
therealwilldecker <willie_decker@yahoo.com>
2015-09-21 13:58:00 -0700

    Update custom.js

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -104,7 +104,7 @@ function initialize() {
           myStereoPhotosphere.setOptions(myPOV);
           myNewPano = getOurPhotosphere(photosphere.pano);
           if(myNewPano != null){
-            myStereoPhotosphere.setOptions({position: myNewPano.location.pano}); // this could actually be the culprit. May have to sync link selection.
+            myStereoPhotosphere.setStreetView({panorama: myNewPano}); // this could actually be the culprit. May have to sync link selection.
           }
         }, 50);
       } else {
