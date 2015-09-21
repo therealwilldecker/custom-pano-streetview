e3ead6b3a7525ebed4d0f8e60927efa1a316ce06
therealwilldecker <willie_decker@yahoo.com>
2015-09-21 11:32:37 -0700

    Update custom.js

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -102,8 +102,8 @@ function initialize() {
   
         setInterval(function(){
           myStereoPhotosphere.setOptions(myPOV);
-          myNewPano = myStereoPhotosphere.panoProvider(photosphere.pano);
-          myStereoPhotosphere.setOptions(myNewPano);
+          myNewPano = getOurPhotosphere(photosphere.pano);
+          myStereoPhotosphere.setOptions(myNewPano); // this could actually be the culprit. May have to sync link selection.
         }, 50);
       } else {
         document.getElementById('map-horizontal-2').style.display='none';
