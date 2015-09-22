2ceb3d365e1162aef3ec293ced5c11dbac1f1bb0
therealwilldecker <willie_decker@yahoo.com>
2015-09-21 21:25:23 -0700

    Update custom.js

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -6,12 +6,17 @@ for (var i = 0; i < welcomeArray.length; i++ ) {
   console.log(welcomeArray[i]);
 }
 
+// Global photosphere info that will be shared across our split screens 
+// @ photosphere, object, for our first and original streetview
+// @ myMapOptions, object, are set mainly to start our streetview with an existing photo
+// @ photoOptions, object, are set mainly to make the streetview visible
+// @ radius, integer, might as well set it :) This is used to grab the pano nearest our starting point
+// @ zoomLevel, integer, is the starting custom zoom level for the map. 0 is zoomed out to the max.
 var photosphere;
 var myMapOptions;
 var photoOptions;
 var radius = 50;
-
-var letsStartOurWalk = null;
+var zoomLevel = 0;
 
 function initialize() {
   // The latlong of our starting point for the custom street view.
@@ -19,7 +24,7 @@ function initialize() {
   
   // OK let's setup the map and start up the controls
   myMapOptions = {
-    center: letsStartOurWalk,
+    center: null,
     zoom: 16
     };
   var ourMap = new google.maps.Map(document.getElementById('map-canvas'),
@@ -41,7 +46,7 @@ function initialize() {
   var streetViewService = new google.maps.StreetViewService();
   
   // Compute the nearest photosphere to our starting point
-  // using the service and store that ID.
+  // using the service.
   streetViewService.getPanoramaByLocation(photoOptions.position, radius,
     function(result, status) {
       if(status == google.maps.StreetViewStatus.OK) {
@@ -49,27 +54,32 @@ function initialize() {
         // photosphere is either a custom one or the starting one
         google.maps.event.addListener(photosphere, 'links_changed',
           function() {
-            photosphere.setZoom(0);
+            photosphere.setZoom(zoomLevel);
             makeMyLinks(photosphere, result.location.pano);
-          });
+          }
+        );
       }
-    });
+    }
+  );
+  return false;
 }
 
 function initializeStereo() {
+  // check first to see if we have a smartphone that can be flip flopped around.
   if(window.orientation != undefined){
     var alpha = 0;
     var beta = 0;
     var gamma = 0;
     
     window.ondeviceorientation = function(event){
+      // Scan for movement of the phone.
       alpha = Math.round(event.alpha);
       beta = Math.round(event.beta);
       gamma = Math.round(event.gamma);
     }
     
+    // Update new coordinates of the view based on where the phone is.
     var myPOV;
-    
     setInterval(function(){
         myPOV = {
           pov: {
@@ -80,32 +90,42 @@ function initializeStereo() {
         photosphere.setOptions(myPOV)
     }, 25);
     
+    // OK let's use some Web Virtual Reality!
+    // Split our screen when landscape and
+    // Sync the maps
     if (window.innerHeight < window.innerWidth) {
       document.getElementById('map-canvas').style.width = '50%';
+      // Let's reset the whole original map size
+      // and keep those zoom levels in sync
       initialize();
       document.getElementById('map-horizontal-2').style.display = 'block';
+      
+      // Setting up another streetview with another getter
       var ourStereoMap = new google.maps.Map(document.getElementById('map-horizontal-2'),
         myMapOptions);
         
       myStereoPhotosphere = ourStereoMap.getStreetView();
       myStereoPhotosphere.setOptions(photoOptions);
 
+      // Starting up a new service so we can calculate
+      // the nearest pano and register our link updater
       var streetViewService = new google.maps.StreetViewService();
       
       streetViewService.getPanoramaByLocation(photoOptions.position, radius,
         function(result, status) {
           if(status == google.maps.StreetViewStatus.OK) {
-            // Monitor the links_changed event to check if the current
-            // photosphere is either a custom one or the starting one
             google.maps.event.addListener(myStereoPhotosphere, 'links_changed',
               function() {
-                myStereoPhotosphere.setZoom(0);
+                myStereoPhotosphere.setZoom(zoomLevel);
                 makeMyLinks(myStereoPhotosphere, result.location.pano);
               });
           }
         });
 
       setInterval(function(){
+        // Monitor the phone position and sync up the panos
+        // Here we just check the original pano and then update the
+        // stereo map to match it.
         myStereoPhotosphere.setOptions(myPOV);
         myNewPano = getOurPhotosphere(photosphere.pano);
         if(myNewPano != null){
@@ -113,15 +133,18 @@ function initializeStereo() {
         }
       }, 25);
     } else {
+      // If we flip flop back up straight, let's reset and turn
+      // off our VR.
       document.getElementById('map-horizontal-2').style.display='none';
       document.getElementById('map-canvas').style.width = '100%';
       initialize();
     }
   }
+  return false;
 }
 
 function getOurPhotosphere(pano) {
-  // this will provide a new photosphere to streetview
+  // this will provide a new photosphere to streetviews
   switch(pano){
     case 'myFavoriteView':
       return {
@@ -153,7 +176,6 @@ function getOurPhotosphere(pano) {
         tiles: {
           tileSize: new google.maps.Size(1024, 512),
           worldSize: new google.maps.Size(1024, 512),
-          // The heading at the origin of the photosphere
           centerHeading: 280,
           getTileUrl: function() {
             return 'https://farm6.staticflickr.com/5712/21505883091_cd24bcf217_b.jpg';
@@ -172,7 +194,6 @@ function getOurPhotosphere(pano) {
         tiles: {
           tileSize: new google.maps.Size(1024, 512),
           worldSize: new google.maps.Size(1024, 512),
-          // The heading at the origin of the photosphere
           centerHeading: 280,
           getTileUrl: function() {
             return 'https://farm1.staticflickr.com/295/18451789334_da965eb5bc_b.jpg';
@@ -191,7 +212,6 @@ function getOurPhotosphere(pano) {
         tiles: {
           tileSize: new google.maps.Size(1024, 512),
           worldSize: new google.maps.Size(1024, 512),
-          // The heading at the origin of the photosphere
           centerHeading: 280,
           getTileUrl: function() {
             return 'https://farm1.staticflickr.com/303/18451845364_97439f775d_b.jpg';
@@ -269,6 +289,7 @@ function makeMyLinks(photosphere, letsStartOurWalk) {
   }
 }
 
+// run our photosphere functions!
 google.maps.event.addDomListener(window, 'load', initialize);
 window.addEventListener("orientationchange", function(){
     initializeStereo();
